0f5de9d446c2f0a89a06dabc877375da22f97b0a
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-04-11 16:12:42 +0100

    add solutions for chat-server

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,14 +1,24 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+  from: 'Bart',
+  text: 'Welcome to CYF chat system!',
 };
 
 //This array is our "data store".
@@ -16,8 +26,107 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT);
+app.get('/messages/:id', (req, res) => {
+  const { id } = req.params;
+  const msg = messages.find(message => message.id === parseInt(id));
+
+  if (msg !== undefined) {
+    res.json(msg);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.delete('/messages/:id', (req, res) => {
+  const { id } = req.params;
+  let index = messages.findIndex(message => message.id === parseInt(id));
+
+  if (index === -1) {
+    res.status(404).send();
+  } else {
+    messages.splice(index, 1);
+    res.status(200).send();
+  }
+});
+
+app.put('/messages/:id', (req, res) => {
+  const { id } = req.params;
+  let newMsg = {
+    text : req.body.text
+  }
+  let msg = messages.find(message => message.id === parseInt(id));
+  if (msg !== undefined && newMsg.text !== undefined) {
+    msg.text = newMsg.text;
+    res.status(200).send();  
+  } else if (msg === undefined) {
+    res.status(404).send();
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.get('/messages', (req, res) => {
+  const { text, sort, limit } = req.query;
+  let foundMessages = messages.slice();
+
+  //filter for the text
+  if (text !== undefined) {
+    foundMessages = messages.filter(msg => msg.text.includes(text));
+  }
+
+  //sort by the id
+  if (sort !== undefined && sort === 'id') {
+    foundMessages.sort(function (msg1, msg2) {
+      return msg2.id - msg1.id;
+    });
+  }
+
+  //limit the messages
+  if (limit !== undefined) {
+    foundMessages = foundMessages.slice(0, Number(limit));
+  }
+
+  res.json(foundMessages);
+});
+
+app.post('/messages', (req, res) => {
+
+  let newMsg = {
+    from: req.body.from,
+    text: req.body.text
+  }
+
+  if (validateNewMsg(newMsg)) {
+    newMsg.id = createId();
+    newMsg.timeSent = new Date().toISOString();
+    messages.push(newMsg);
+    res.status(200).send();
+  } else {
+    res.status(400).send();
+  }
+});
+
+function validateNewMsg(newMsg) {
+  if (
+    newMsg !== undefined && 
+    newMsg.from !== undefined &&
+    newMsg.from.length !== 0 &&
+    newMsg.text !== undefined &&
+    newMsg.text.length !== 0
+  )
+    return true;
+  return false;
+}
+
+function createId() {
+  let newId = messages[messages.length - 1].id + 1;
+  return newId;
+}
+
+app.listen(port, () =>
+  console.log(`[MockServer] listening at http://localhost:${port}`)
+);
