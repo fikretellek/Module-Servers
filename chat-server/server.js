5908b5feefbb2782fddb415094f3c7d7befd8e2c
anuthapaliy <113211321+anuthapaliy@users.noreply.github.com>
2023-07-29 15:09:45 +0100

    almost done

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,7 +1,7 @@
 process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
 const cors = require("cors");
-const { response } = require("express");
+const { response, text } = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -28,28 +28,55 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-// - [ ] Create a new message
-app.post("/messages", (request, response) => {
-  const newMessage = {
-    "id": messages.length,
-    "from": request.body.from,
-    "text": request.body.text,
-    // Level 4 challenge- Add the current time as the timeSent property
-    // "timeSent": new Date() 
-  };
 
-  messages.push(newMessage);
-  response.status(201).json(newMessage);
+
+// (1st challenge -create a new message) and challenges 2-create a new messages with validation-:
+app.post("/messages", (request, response) => {
+  console.log("posting");
+
+  const {from, text } = request.body;
+  if(!from || !text || from.trim() === "" || text.trim() === "") {
+    console.log("invalidMessage")
+    console.log(request.body)
+    response.status(400).json({ error: "Missing or empty 'from' or 'text' property"});
+  } else {
+    const newMessage = {
+      id: messages.length,
+      from: from,
+      text: text,
+    };
+
+    messages.push(newMessage);
+    response.status(201).json(newMessage);
+  }
 });
+
 
  // - [ ] Read all messages
 app.get("/messages", (request, response) => {
   response.json(messages);
 });
 
+// Level-3-challenges
+  // - [ ] Read _only_ messages whose text contains a given substring: `/messages/search?text=express`
+app.get("/messages/search", (request, response) => {
+
+  console.log("Anu")
+    const searchText = request.query.text.toLowerCase();
+    console.log(searchText);
+    const filteredMessages = messages.filter(
+      (msg) => msg.text.toLowerCase().includes(searchText)
+    );
+  
+    response.json(filteredMessages);
+  });
+
 // - [ ] Read one message specified by an ID
 app.get("/messages/:id", (request, response) => {
+  console.log("Sal")
+
   const messageId = parseInt(request.params.id);
+  console.log(request.params.id);
   // Find the message with the specified ID in the messages array
   const message = messages.find((msg) => msg.id === messageId);
 if (message) {
@@ -59,7 +86,8 @@ if (message) {
 }
 });
 // - [ ] Delete a message, by ID
-  app.delete("/message/:id", (request, response) => {
+  app.delete("/messages/:id", (request, response) => {
+    console.log(request.params.id)
     const messageId = parseInt(request.params.id);
     // Find the index of the message with the specified ID
   const messageIndex = messages.findIndex((msg) => msg.id === messageId);
@@ -74,34 +102,8 @@ if (message) {
 }
 });
 
-// challenges 2-create a new messages with validation-:
-app.post("/messages", (request, response) => {
-  const {from, text } = request.body;
-  if(!from || !text || from.trim() === "" || text.trim() === "") {
-    response.status(400).json({ error: "Missing or empty 'from' or 'text' property"});
-  } else {
-    const newMessage = {
-      id: messages.length,
-      from: from,
-      text: text,
-    };
 
-    messages.push(newMessage);
-    response.status(201).json(newMessage);
-  }
-});
 
-// Level-3-challenges
-  // - [ ] Read _only_ messages whose text contains a given substring: `/messages/search?text=express`
-app.get("/messages/search", (request, response) => {
-  const searchText = request.query.text.toLowerCase();
-
-  const filteredMessages = messages.filter(
-    (msg) => msg.text.toLowerCase().includes(searchText)
-  );
-
-  response.json(filteredMessages);
-})
 
 // Read the most recent 10 messages
 app.get("/messages/latest", (request, response) => {
