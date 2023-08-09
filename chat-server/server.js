56c447dc00f8ff0a31a523496b83af5282309068
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-08-09 19:40:20 +0100

    New Message Done

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,12 +5,36 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
+
+let nextMessageId = 1;
+
+const messages = [welcomeMessage];
+
+
+app.post("/messages", function (req, res) {
+
+})
+
+app.get("/messages",  function(req, res) {
+
+})
+
+app.get("/messages/:id", function(req, res) {
+
+})
+
+app.delete("/messages/:id", function (req, res) {
+
+})
+
+
 
 //This array is our "data store".
 //We will start with one message in the array.
