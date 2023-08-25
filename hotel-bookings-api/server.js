fb4a83c9a9d89737496ac425c78a84cedc1f6511
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-08-25 18:22:19 +0100

    Update server.js

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const PORT = 3000;
+const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
@@ -15,6 +18,76 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/bookings", function (req, res) {
+  res.json(dataStore);
+});
+
+app.get("/bookings/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  const booking = dataStore.find((booking) => booking.id === id);
+
+  if (booking) {
+    res.json(booking);
+  } else {
+    res.status(404).json({ error: "Booking not found" });
+  }
+});
+
+app.post("/bookings", function (req, res) {
+  const newBooking = req.body;
+
+  if (!isValidBooking(newBooking)) {
+    res.status(400).json({ error: "Invalid booking data" });
+  } else {
+    newBooking.id = generateNewId();
+    dataStore.push(newBooking);
+    res.status(201).json(newBooking);
+  }
+});
+
+app.delete("/bookings/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  const index = dataStore.findIndex((booking) => booking.id === id);
+
+  if (index !== -1) {
+    dataStore.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    res.status(404).json({ error: "Booking not found" });
+  }
+});
+
+app.get("/bookings/search", function (req, res) {
+  const date = req.query.date;
+  const matchingBookings = dataStore.filter(
+    (booking) =>
+      booking.checkInDate <= date && booking.checkOutDate >= date
+  );
+
+  res.json(matchingBookings);
+});
+
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+function generateNewId() {
+  const ids = dataStore.map((booking) => booking.id);
+  return Math.max(...ids) + 1;
+}
+
+function isValidBooking(booking) {
+  return (
+    booking.roomId &&
+    booking.title &&
+    booking.firstName &&
+    booking.surname &&
+    booking.email &&
+    booking.checkInDate &&
+    booking.checkOutDate
+  );
+}
