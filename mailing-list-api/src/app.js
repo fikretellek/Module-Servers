d0234cab002f158ad5e123635053ea59f31f229d
fikretellek <fikretellek@icloud.com>
2024-05-24 16:32:33 +0100

    updated info message

--- mailing-list-api/src/app.js
+++ mailing-list-api/src/app.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send(
+    'please go to \n/list ---> for all group names \n/list/:name ---> for mail list of a group of members\nyou can delete or update a group as well \n(please provide group name and members for update method as below)\n ---> {"name": "group-name","members": ["me@me.com", ...]}'
+  );
+});
 app.use("/lists", readFileMiddleware(resolveFilePath()), allListRouter);
 
 export { app };
